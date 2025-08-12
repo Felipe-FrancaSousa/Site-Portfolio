5dd7394d41a8bc7bc1aa9dadc3bd2462ce5181d7
-Felipe França <felipefrancasousa@gmail.com>
2025-08-12 18:02:18 -0300

    Atualização do Display Grid
    Versão 0.2 do site onde mantenho o Grid como modo de exibir as caixas, ainda mantenho o código do Display Flex comentado no final do CSS

--- data/data.php
+++ data/data.php
@@ -3,7 +3,7 @@ $data=[
     [
         'id' => 1,
         'name' => 'Foto 1',
-        'img' => 'foto1.jpg',
+        'img' => 'foto6.jpg',
     ],
     [
         'id' => 2,
@@ -13,7 +13,7 @@ $data=[
     [
         'id' => 3,
         'name' => 'Foto 3',
-        'img' => 'foto3.jpg',
+        'img' => 'foto6.jpg',
     ],
     [
         'id' => 4,
@@ -28,7 +28,7 @@ $data=[
     [
         'id' => 6,
         'name' => 'Foto 6',
-        'img' => 'foto2.jpg',
+        'img' => 'foto6.jpg',
     ],
     [
         'id' => 7,
@@ -43,7 +43,7 @@ $data=[
         [
         'id' => 1,
         'name' => 'Foto 1',
-        'img' => 'foto1.jpg',
+        'img' => 'foto6.jpg',
     ],
     [
         'id' => 2,
@@ -58,12 +58,12 @@ $data=[
     [
         'id' => 4,
         'name' => 'Foto 4',
-        'img' => 'foto4.jpg',
+        'img' => 'foto6.jpg',
     ],
         [
         'id' => 5,
         'name' => 'Foto 5',
-        'img' => 'foto1.jpg',
+        'img' => 'foto6.jpg',
     ],
     [
         'id' => 6,
@@ -73,12 +73,22 @@ $data=[
     [
         'id' => 7,
         'name' => 'Foto 7',
-        'img' => 'foto3.jpg',
+        'img' => 'foto6.jpg',
     ],
     [
         'id' => 9,
         'name' => 'Foto 9',
-        'img' => 'foto4.jpg',
+        'img' => 'foto1.jpg',
+    ],
+    [
+        'id' => 7,
+        'name' => 'Foto 7',
+        'img' => 'foto6.jpg',
+    ],
+    [
+        'id' => 9,
+        'name' => 'Foto 9',
+        'img' => 'foto1.jpg',
     ],
 ]
 ?>
--- index.php
+++ index.php
@@ -1,14 +1,15 @@
 <?php include_once("templates/header.php");?>
-
-    <div class="topo">
-        <h1>Halfafa tropical!</h1>
-    </div>
-    <div class="conteudo">
-        <?php foreach($data as $post):?>
-            <div class="caixa-post">
-                <img src="<?=$BASE_URL?>/img/<?=$post['img']?>" alt="<?=$post['name']?>">
-            </div>
-        <?php endforeach; ?>
-    </div>
-
+    <main>
+        <div class="topo">
+            <h1>Halfafa tropical!</h1>
+        </div>
+        <div class="conteudo">
+            <?php foreach($data as $post):?>
+                <div class="caixa-post" >
+                    <img src="<?=$BASE_URL?>/img/<?=$post['img']?>" alt="<?=$post['name']?>">
+                    <h1><?=$post['name']?></h1>
+                </div>
+            <?php endforeach; ?>
+        </div>
+    </main>
 <?php include_once("templates/footer.php");?>
